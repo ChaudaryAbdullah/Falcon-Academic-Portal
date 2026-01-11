9f4cf06279023d987a947abc8fb334e488bb11ec
ChaudaryAbdullah <abdullahshakir226@gmail.com>
2026-01-11 16:20:24 +0500

    lazy load removed

--- backend/index.js
+++ backend/index.js
@@ -55,7 +55,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-// app.options("*", cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 // Middleware
 app.use(express.json({ limit: "50mb" }));
